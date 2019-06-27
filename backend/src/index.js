b77f74259bff4216163e16646382065c03f0bb6b
amelco <amelco.herman@gmail.com>
2019-06-27 18:12:54 -0300

    add fix to CORS error

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// Soluciona erro relacionado com CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 mongoose.connect('mongodb://localhost:27017/mongoDB', {
     useNewUrlParser: true,
 });
