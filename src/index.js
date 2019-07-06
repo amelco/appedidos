16db30f8e458f3ed9a5ef8fb84d119c28667083a
Andre Herman <amelco.herman@gmail.com>
2019-07-06 14:27:14 -0300

    Add enviroment variable to deal with MongoDB access (dotenv package)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 
 // Soluciona erro relacionado com CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-// using Atlas
-mongoose.connect('mongodb+srv://semana:semana@cluster0-krc4r.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true,
 });
 
-// using local server (develoopment only)
+// using local server (development only)
 // mongoose.connect('mongodb://localhost:27017/mongoDB', {
 //     useNewUrlParser: true,
 // });
@@ -23,6 +26,6 @@ mongoose.connect('mongodb+srv://semana:semana@cluster0-krc4r.mongodb.net/test?re
 app.use(express.urlencoded());
 app.use(require('./routes'));
 
-const port = process.env.PORT || 3333;
-console.log(port);
+const port = process.env.PORT;
+
 app.listen(port);
